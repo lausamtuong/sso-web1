e6f4a87e0dfc88ee3367fbbc2366349c3e3b330b
lausamtuong <lausamtuong@gmail.com>
2023-11-05 23:41:12 +0700

    refactor flow cookie in saml

--- src/pages/login/success/index.tsx
+++ src/pages/login/success/index.tsx
@@ -1,15 +1,8 @@
-import { setCookie } from '@/lib/cookie';
-import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
 const LoginSuccess = () => {
-  const searchParams = useSearchParams();
-  const email = searchParams.get('email');
-  setCookie('email', String(email));
-
   useEffect(() => {
     setTimeout(() => {
-      setCookie('isAuthen', 'TRUE');
       window.close();
     }, 1000);
   }, []);
